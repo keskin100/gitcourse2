1fee01799ba5b5e415cf5da95f0f7e090cdc959e
furkan okcu <archerfurkan1@gmail.com>
2021-04-03 13:49:09 +0300

    furkan

---
+++ src/com/koray/furkan.java
@@ -0,0 +1,9 @@
+package com.koray;
+
+public class furkan {
+    double digit1;
+    double digit2;
+    public static double sum(double digit1,double digit2){
+        return digit1+digit2;
+    }
+}
